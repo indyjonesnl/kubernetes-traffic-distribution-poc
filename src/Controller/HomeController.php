c9c309368c59ab964e2af9f43bf2ce73860255ef
Indy Jones <indyjonesnl@gmail.com>
2026-01-14 15:04:55 +0100

    Add route for getting node names

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,6 +13,14 @@ use Throwable;
 
 final class HomeController extends AbstractController
 {
+    private const int NUM_CALLS = 6;
+
+    public function __construct(
+        private readonly HttpClientInterface $hostnameClient,
+    )
+    {
+    }
+
     #[Route('/healthz', methods: [Request::METHOD_GET])]
     public function healthz(): Response
     {
@@ -20,27 +28,42 @@ final class HomeController extends AbstractController
     }
 
     #[Route('/', methods: [Request::METHOD_GET])]
-    public function home(HttpClientInterface $hostnameClient): Response
+    public function home(): Response
+    {
+        $results = $this->callMicroservices(self::NUM_CALLS, false);
+
+        return $this->json($results);
+    }
+
+    private function callMicroservices(int $amount, bool $useNodeName): array
     {
         $responses = [];
-        for ($index = 0; $index < 6; $index++) {
+        for ($index = 0; $index < $amount; $index++) {
             // send all requests concurrently (they won't block until response content is read)
-            $responses[$index] = $hostnameClient->request(Request::METHOD_GET, '/api/hostname');
+            $responses[$index] = $this->hostnameClient->request(Request::METHOD_GET, $useNodeName ? '/api/node_name' : '/api/hostname');
         }
 
         $results = [
-            'webserver' => gethostname(),
+            'webserver' => $useNodeName ? getenv('NODE_NAME') : gethostname(),
         ];
 
         // iterate through the responses and read their content
         foreach ($responses as $index => $response) {
             // process response data somehow ...
             try {
-                $results['microservice-' . ($index+1)] = $response->toArray()['hostname'];
+                $results['microservice-' . ($index+1)] = $response->toArray()[$useNodeName ? 'node_name' : 'hostname'];
             } catch(Throwable $t) {
                 var_dump($t);
             }
         }
+
+        return $results;
+    }
+
+    #[Route('/node_name', methods: [Request::METHOD_GET])]
+    public function nodeName(): Response
+    {
+        $results = $this->callMicroservices(self::NUM_CALLS, true);
 
         return $this->json($results);
     }
